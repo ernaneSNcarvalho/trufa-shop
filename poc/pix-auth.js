c6dd6214348fd42473bce4d4396bdadad6772828
ernane <ernanecarvalho89@gmail.com>
2022-02-03 14:07:35 -0300

    finish

--- poc/pix-auth.js
+++ poc/pix-auth.js
@@ -1,2 +1,44 @@
 require('dotenv').config({path: '.env.homologacao'});
 console.log(process.env.GN_ENV);
+
+const https = require('https')
+const axios = require('axios')
+const fs = require('fs')
+
+const apiProduction = 'https://api-pix.gerencianet.com.br'
+const apiStaging = 'https://api-pix-h.gerencianet.com.br'
+
+const baseUrl = process.env.GN_ENV === 'producao' ? apiProduction : apiStaging
+
+const getToken = async () => {
+    console.log('getToken', process.env.GN_ENV)
+    const certificado = fs.readFileSync('../' + process.env.GN_CERTIFICADO)
+    const credenciais = {
+        client_id: process.env.GN_CLIENT_ID,
+        client_secret: process.env.GN_CLIENT_SECRET
+    }
+    const data = JSON.stringify({grant_type: 'client_credentials'})
+    const dataCredenciais = credenciais.client_id + ':' + credenciais.client_secret
+    const auth = Buffer.from(dataCredenciais).toString('base64')
+
+    const agent = new https.Agent({
+        pfx: certificado, 
+        passphrase: ''
+    })
+
+    const config = {
+        method: 'POST',
+        url: baseUrl + '/oauth/token',
+        headers: {
+            Authorization: 'Basic' + auth,
+            'Content-Type': 'application/json'
+        },
+        httpsAgent: agent,
+        data: data
+
+    }
+
+    const result = await axios(config)
+    console.log(result)
+}
+getToken()
